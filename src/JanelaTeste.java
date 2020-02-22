5776221b75bcc548e4a1097970c6a2e31b62b875
gustavo-bertoldi <58533109+gustavo-bertoldi@users.noreply.github.com>
2020-02-22 17:48:58 +0100

    Falta o straight

--- src/JanelaTeste.java
+++ src/JanelaTeste.java
@@ -7,14 +7,20 @@ public class JanelaTeste extends JFrame {
 
 
     private JPanel principal = new JPanel(new BorderLayout());
-    private JPanel cartasJogador = new JPanel(new FlowLayout());
+    private JPanel cartasJogadorEsquerda = new JPanel(new FlowLayout());
+    private JPanel cartasJogadorDireita = new JPanel(new FlowLayout());
+    private JPanel cartasJogadorTopo = new JPanel(new GridLayout(1,3));
+    private JPanel topo1 = new JPanel(new FlowLayout());
+    private JPanel topo2 = new JPanel(new FlowLayout());
+    private JPanel topo3 = new JPanel(new FlowLayout());
+    private JPanel cartasJogadorBaixo = new JPanel(new FlowLayout());
     private JPanel mesa = new JPanel(new FlowLayout());
 
 
 
     public JanelaTeste(){
         super("Poker");
-        this.setSize(1200 ,1200);
+        this.setSize(1200 ,400);
         jogo.distribuirCartas();
 
         for(Carta c: jogo.getMesa()){
@@ -22,10 +28,38 @@ public class JanelaTeste extends JFrame {
         }
 
         for(Carta c:jogo.getJogadores().get(0).getHand()){
-            cartasJogador.add(new JButton(c.icone));
+            cartasJogadorBaixo.add(new JButton(c.icone));
         }
 
-        this.add(cartasJogador, BorderLayout.SOUTH);
+        for(Carta c:jogo.getJogadores().get(1).getHand()){
+            cartasJogadorEsquerda.add(new JButton(c.icone));
+        }
+
+        for(Carta c:jogo.getJogadores().get(2).getHand()){
+            topo1.add(new JButton(c.icone));
+        }
+
+        for(Carta c:jogo.getJogadores().get(3).getHand()){
+            topo2.add(new JButton(c.icone));
+        }
+
+        for(Carta c:jogo.getJogadores().get(4).getHand()){
+            topo3.add(new JButton(c.icone));
+        }
+
+        for(Carta c:jogo.getJogadores().get(5).getHand()){
+            cartasJogadorDireita.add(new JButton(c.icone));
+        }
+
+
+        this.add(mesa, BorderLayout.CENTER);
+        cartasJogadorTopo.add(topo1);
+        cartasJogadorTopo.add(topo2);
+        cartasJogadorTopo.add(topo3);
+        this.add(cartasJogadorBaixo, BorderLayout.SOUTH);
+        this.add(cartasJogadorEsquerda, BorderLayout.WEST);
+        this.add(cartasJogadorTopo, BorderLayout.NORTH);
+        this.add(cartasJogadorDireita, BorderLayout.EAST);
         this.add(mesa, BorderLayout.CENTER);
 
 
