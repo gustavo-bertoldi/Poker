a6f22845c746970b37107d6dafcc427433f66cd6
matiaskdr <50099189+matiaskdr@users.noreply.github.com>
2020-05-08 16:03:22 +0200

    Add files via upload

--- src/Intelligence.java
+++ src/Intelligence.java
@@ -1,83 +1,243 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
+public class Intelligence{
 
-
-public class Intelligence {
-
-    private int niveau; // pris de Jeu
-    protected double[] rangeBet = new double[2];
-    protected double[] rangeCall = new double[2]; // pour quelles bets il va payer
-    protected double[] rangeDecisions = new double[2];
-    private LinkedList<Carte> surMain = new LinkedList<>(); // les cartes distribuees au debut du jeu
-    private LinkedList<Carte> surTable = new LinkedList<>(); //affectée en ajoutant les cartes tournees
-    protected Hand hand; // protected pour pouvoir affecter depouis joueur; y a til une façon de l'affecter directement?
-    private Joueur joueur;
-
-    /*
-    - qualiteMain mesure en pourcentage le pouvoir de la main
-    - est affecté à chaque round
-    - prend en compte les valeurs et position des cartes pour evaluer la qualité
-    - prendre en compte les possibilites des autres mains si niveau haut
-     */
-    protected double qualiteMain = 0; // en quelle mesure est la hand bonne pourcentage
-
-    /*
-    HighCard[0;0,1] 0,1 si Ace 2->14
-    OnePair[0,1;0,3] 0,2 si pair de aces 20->140
-    TwoPairs[0,3;0,45] 0,3 si aces et rois 200->2700 pair*10 +pair2*10
-    ThreeOfAKind[0,45;0,6] 0,4 si aces 20000->140000
-    Straight[0,6;0,75] 0,5 si roi kicker
-    Flush [0,75;0,85] 0,6 si roi kicker
-    FullHouse [0,85;0,9] 0,7 si trois aces
-    FourOfAKind[0,9;0,95] 0,8 si aces
-    StraightFlush[0,95;1] 1 si royalStraight
-    RoyalStraightFlush[1]
-     */
-
-    /*
-    La prise de decisions prend en compte les pourcentages de qualite de main.
-    Le niveau d'intelligence change à partir de quel pourcentage on prend telle ou telle decision
-    Mais change aussi le blef
-     */
-    private ArrayList<Integer> blef = new ArrayList<>();// Array pour avoir contains()
-
-    public Intelligence(int i, Joueur j, Hand hand) {
-        niveau = i; //i donne par Jeu
-        this.joueur = j;
-        this.hand = hand;
-    }
-   /* public void setSurMain(LinkedList<Carte> surMain){
-        this.surMain = surMain;
-        hand.setSurMain(surMain);
-
-    public void setSurTable(LinkedList<Carte> tourneesSurTable){
-        surTable.addAll(tourneesSurTable); // ajoute toutes les cartes prises comme parametre à surTable
-        hand.setHand(tourneesSurTable);
-    }*/
-
-    public boolean Jouable() {// methode pour sortir si on a pas mieu que hotaur as
-        int v = 0;
-        v = hand.getValeurHand();
-        if (v < 15) {
-            return false;
+    public static int getDecision(Jeu jeu, Joueur joueur, int niveau){
+        if(niveau==0){
+            return intelligenceAleatoire(jeu,joueur);
         }
-        return false;
+        else if(niveau==1){
+            return intelligenceNiveau1(jeu,joueur);
+        }
+        else if(niveau==2){
+            return intelligenceNiveau2(jeu,joueur);
+       } else{
+            return -2;
+        }
     }
 
-    public int decision(int moment){
-        int decision = 0;
-        if(niveau==0) {
-            int puissance = (int)(Math.random()*10);
-            decision = (int)((Math.random()*Math.PI/2)*Math.pow(-1,puissance));
+    private static int intelligenceAleatoire(Jeu jeu, Joueur joueur){
+        int decision;
+        if(joueur.allIn){
+            decision=1;
+        }
+
+        else if((jeu.pariActuel-joueur.derniereValeurPariee)==0){
+            int r = (int) (Math.random() * 30);
+            if(r<=5){
+                int raiseRange = (int) (50 * Math.random());
+                int raise;
+                if (raiseRange <= 1) {
+                    raise = joueur.getArgent();
+                } else if (raiseRange < 40) {
+                    raise = 50;
+                } else {
+                    raise = 100;
+                }
+                decision=raise;
+            }
+            else{
+                decision=1;
+            }
+        }
+
+        else {
+            int r = (int) (Math.random() * 30);
+            if (r <= 10) {
+                decision=0;
+            } else if (r <= 26) {
+                decision=1;
+            } else {
+                if (jeu.pariActuel <= joueur.getArgent() / 10) {
+                    int raiseRange = (int) (50 * Math.random());
+                    int raise;
+                    if (raiseRange <= 2) {
+                        raise = joueur.getArgent();
+                    } else if (raiseRange < 35) {
+                        raise = 2 * jeu.pariActuel;
+                    } else {
+                        raise = 4 * jeu.pariActuel;
+                    }
+                    decision=raise;
+                } else {
+                    decision=1;
+                }
+            }
+        }
+        return decision;
+    }
+
+    private static int intelligenceNiveau1 (Jeu jeu, Joueur joueur){
+        int decision;
+        Joueur j = joueur;
+
+        if (!jeu.flop) { //quand le joueur a seulement ses 2 cartes à lui
+			
+
+           if (j.getHand().getValeurHandSurMain() > 14) { //si le joueur a au moins une paire, il va doubler le pari
+                decision =  2 * jeu.pariActuel;
+           }
+
+           else if (j.bigBlind) { //si le joueur est le Big Blind, il va toujours passer au moment 1
+               decision = 1;
+           }
+
+           else if (jeu.pariActuel < (1 / 6) * j.getArgent()) { //si le pari actuel est peu important en comparaison à l´argent du joueur, le joueur paye toujours le pari
+                decision = 1;
+
+           }
+
+           else { //si le pari actuel est assez important
+                if (j.getHand().getValeurHandSurMain() >= 8) { //si le joueur a une carte supérieure ou égale à 8 (et pas de paires ni rien meilleur), il paye le parie. Sinon, il arrête de jouer.
+                    decision = 1;
+                } else {
+                    decision = 0;
+                }
+           }
+           
+        }
+
+
+        else if (!jeu.turn) { //quand les 3 premières cartes sont révélées
+			
+
+            if (j.getHand().getValeurHandApresFlop() < 8 && jeu.pariActuel > 0) { //si le joueur était le Big Blind mais il n´a que des cartes inférieures à 8 (et pas de paires ni rien meilleur), il arrête de jouer
+                decision = 0;
+            }
+
+           else if (j.getHand().getValeurHandApresFlop() < 60) { //si le joueur a une paire de 5 ou une Hand moins importante, il paye le pari actuel. Si il a une Hand plus importante, il augmente de 100 le pari actuel.
+                decision = 1;
+            } else {
+				if(j.derniereValeurPariee < 100){
+                decision = jeu.pariActuel + 100;
+			} else {
+				decision = 1;
+			}
+            }
+               
+             
+        }
+
+        else if (!jeu.river) { //quand il y a 4 cartes révélées
+			
+			
+            if(j.allIn){
+                decision=1;
+            }
+
+            else if (j.getHand().getValeurHandApresTurn() == j.getHand().getValeurHandApresFlop()) {  //si le joueur n´a pas une meilleure Hand qu´au moment précédent, le joueur paye le pari actuel
+                decision = 1;
+
+            } else {
+				if(j.derniereValeurPariee < 100){
+                decision = (jeu.pariActuel + 70); //sinon (sa valeur de Hand a augmenté), il augmente de 70 le pari actuel
+			} else { 
+				decision = 1;
+			}
+		}
+            
+           
+         
+        }
+
+        else { //quand toutes les cartes ont été révélées
+			
+			
+            if(j.allIn){
+                decision=1;
+            }
+            else if (j.getHand().getValeurHandApresRiver() == j.getHand().getValeurHandApresTurn()) { //si le joueur n´a pas une meilleure Hand qu´au moment précédent, le joueur paye le pari actuel
+                decision = 1;
+            } else {
+				if (j.derniereValeurPariee < 100){
+                decision =  jeu.pariActuel + 50; //sinon (sa valeur de Hand a augmenté), il augmente de 50 le pari actuel
+			} else{
+				decision = 1;
+			}
+            }
+             
         }
 
         return decision;
     }
+    
+    private static int intelligenceNiveau2 (Jeu jeu, Joueur joueur){
+        int decision;
+        int valeurMoment2=0;
+        int valeurHand = joueur.getHand().getValeurHandApresRiver();
+
+        if (!jeu.flop) { //quand le joueur a seulement ses 2 cartes à lui
+
+           if (valeurHand > 14) { //si le joueur a au moins une paire, il va doubler le pari
+                decision =  2 * jeu.pariActuel;
+           }
+
+           else if (joueur.bigBlind) { //si le joueur est le Big Blind, il va toujours passer au moment 1
+               decision = 1;
+           }
+
+           else if (jeu.pariActuel < (1 / 10) * joueur.getArgent()) { //si le pari actuel est peu important en comparaison à l´argent du joueur, le joueur paye toujours le pari
+                decision = 1;
+
+           }
+
+           else { //si le pari actuel est assez important
+                if (joueur.getHand().getValeurHandSurMain() >= 8) { //si le joueur a une carte supérieure ou égale à 8 (et pas de paires ni rien meilleur), il paye le parie. Sinon, il arrête de jouer.
+                    decision = 1;
+                } else {
+                    decision = 0;
+                }
+           }
+        }
 
 
-    public void setRangeBet(){
-        // ajouter methode prenant en compte qualite main
+        else if (!jeu.turn) { //quand les 3 premières cartes sont révélées
+
+            if (valeurHand < 8 && jeu.pariActuel > 0) { //si le joueur était le Big Blind mais il n´a que des cartes inférieures à 8 (et pas de paires ni rien meilleur), il arrête de jouer
+                decision = 0;
+            }
+
+            if (joueur.getHand().getValeurHandApresFlop() < 60) { //si le joueur a une paire de 5 ou une Hand moins importante
+                
+                if(jeu.pariActuel > 0.5*joueur.getArgent()){ //si le pari est supérieur à la moitié de l´argent que possède le joueur
+					decision = -1; // il arrête de jouer
+					} else { //sinon (le pari est inférieur à la moitié de son argent)
+					decision = 1; //il paye le pari actuel
+					}
+                
+            } else { //Si il a une Hand plus importante, il augmente de 300 le pari actuel.
+                decision = jeu.pariActuel + 300;
+            }
+
+         
+        }
+
+        else if (!jeu.river) { //quand il y a 4 cartes révélées
+            if(joueur.allIn){
+                decision=1;
+            }
+
+            else if (joueur.getHand().getValeurHandApresTurn() < 230 && jeu.pariActuel > 0) { // si le joueur n´a pas au moins 2 paires, il arrête de jouer
+				decision = -1;
+					
+			} else if (joueur.getHand().getValeurHandApresTurn() < 650){ //sinon (le joueur a au moins deux paires), si les 2 paires sont moyennement fortes, il paye le pari actuel
+				decision = 1; 
+
+            } else { //si le joueur a au moins 2 paires fortes, il augmente de 200 le pari actuel
+                decision = (jeu.pariActuel + 200);  
+            }
+            valeurMoment2 = joueur.getHand().getValeurHandApresTurn(); //variable intermédiaire pour stocker la valeur du Hand au moment 2
+        }
+
+        else { //quand toutes les cartes ont été révélées
+            if(joueur.allIn){
+                decision=1;
+            }
+            else if (joueur.getHand().getValeurHandApresRiver() == valeurMoment2) { //si le joueur n´a pas une meilleure Hand qu´au moment précédent, le joueur paye le pari actuel
+                decision = 1;
+            } else {
+                decision =  jeu.pariActuel + 200; //sinon (sa valeur de Hand a augmenté), il augmente de 200 le pari actuel
+            }
+        }
+
+        return decision;
     }
-
-
 }
